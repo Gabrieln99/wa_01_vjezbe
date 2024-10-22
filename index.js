ff363fbc52cc847daf2a880cbd3a86fa66ab5e47
Gabrieln999 <119957268+Gabrieln999@users.noreply.github.com>
2024-10-22 09:53:04 +0200

    prvi_zadatak_wa_gn

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
+const e = require("express");
 const express = require("express");
+const useri = [
+  { ime: "gabriel", prezime: "nadal" },
+  { ime: "patrik", prezime: "fab" },
+  { ime: "pero", prezime: "peric" },
+];
 
 let app = express();
 
-const PORT = 30;
-
+const PORT = 3000;
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.sendFile(__dirname + "/public/index.html");
 });
 
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + "/public/about.html");
+});
 
-
-app.listen(PORT, (error)=>{
-
-if (error){
-    console.log(error);
-}else {
-  console.log("slušam");}}
-);
+app.get("/users", (req, res) => {
+  res.json(useri);
+});
+app.listen(PORT, (error) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log("slusam na ovom portu:", PORT);
+  }
+});
